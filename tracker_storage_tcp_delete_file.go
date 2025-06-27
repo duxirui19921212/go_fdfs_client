1918f54cb39563c822de775ce912ee81c9fc71f1
duxirui19921212 <duxirui19921212@gmail.com>
2025-06-27 16:54:01 +0800

    Update tracker_storage_tcp_delete_file.go
    修改删除方法TrackerServerTcpClient的作用于为公共

--- tracker_storage_tcp_delete_file.go
+++ tracker_storage_tcp_delete_file.go
@@ -2,7 +2,7 @@ package fastdfs_client_go
 
 //DeleteFile 删除存储在  storage server 服务器的文件
 // @fileId storage server 服务器存储的完整文件名，例如：group1/M00/00/01/MeiRdmIQ7N-AOJmJAAAAD1y-ivQ067.png
-func (c *trackerServerTcpClient) DeleteFile(fileId string) error {
+func (c *TrackerServerTcpClient) DeleteFile(fileId string) error {
 	groupName, remoteFilename, err := splitStorageServerFileId(fileId)
 	if err != nil {
 		return err
