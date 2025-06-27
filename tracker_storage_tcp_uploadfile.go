bc528c555d618d6797ea1938e930549e5fbad830
duxirui19921212 <duxirui19921212@gmail.com>
2025-06-27 16:55:26 +0800

    Update tracker_storage_tcp_uploadfile.go
    修改上传方法TrackerServerTcpClient的作用于为公共

--- tracker_storage_tcp_uploadfile.go
+++ tracker_storage_tcp_uploadfile.go
@@ -2,7 +2,7 @@ package fastdfs_client_go
 
 // UploadByFileName  创建fdfs客户端, 上传文件
 // @fileName 指定已经存在的文件名
-func (c *trackerServerTcpClient) UploadByFileName(fileName string) (string, error) {
+func (c *TrackerServerTcpClient) UploadByFileName(fileName string) (string, error) {
 	file, err := getFileInfoByFileName(fileName)
 	defer file.Close()
 	if err != nil {
@@ -27,7 +27,7 @@ func (c *trackerServerTcpClient) UploadByFileName(fileName string) (string, erro
 // UploadByBuffer  创建fdfs客户端, 上传文件
 // @buffer 二进制数据,适合小文件一次性发送
 // @fileExtName 指定文件在服务器端保存时的文件名
-func (c *trackerServerTcpClient) UploadByBuffer(buffer []byte, fileExtName string) (string, error) {
+func (c *TrackerServerTcpClient) UploadByBuffer(buffer []byte, fileExtName string) (string, error) {
 	tmpFileInfo, err := getFileInfoByFileByte(buffer, fileExtName)
 	defer tmpFileInfo.Close()
 	if err != nil {
